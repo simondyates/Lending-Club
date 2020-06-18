9d47037ce5310be317252026f010122d03d3bb1e
Simon Yates <simon.yates@gmail.com>
2020-06-18 18:26:53 -0400

    adding YieldCurve.py and completed Simulator.py

--- Simon/Simulator.py
+++ Simon/Simulator.py
@@ -15,13 +15,16 @@ from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import GradientBoostingRegressor
 from collections import defaultdict
+from YieldCurve import YieldCurve
+import plotly.graph_objects as go
+import plotly.io as pio
 
 # Global parameters
 lc_fee = .01 # i.e. 1%.  This is charged on all on-time payments.  I'm assuming the numbers we have are gross so we need to subtract this
 rec_lag = 9 # recoveries arrive 'rec_lag' months after last_pymnt_d
 first_invest = 20100101 # Use data before this point to fit the starting model.  This gives us 1 year of data (not a lot).
-refit = 24 # Refit the model every 'refit' months
-est_steps = 30 # Add this many estimators to the model each refit
+refit = 44 # Refit the model every 'refit' months
+est_steps = 60 # Add this many estimators to the model each refit
 
 # Tactic parameters
 max_per_loan = 1000 # USD in any one loan
@@ -31,23 +34,27 @@ min_ret = .1 # The minimum required value of (PV/Principal-1) in order to invest
 
 # Define a class to track a portfolio of LC loans; construct cash-flows for them; and calculate metrics
 class Portfolio:
-    def __init__(self, universe, rates, rec_lag=0):
-        self.__univ = universe # df of all the loan data we have
-        self.__rates = rates # df of historical risk-free rates
+    def __init__(self, universe, yc, rec_lag=0):
+        self.__univ__ = universe # df of all the loan data we have
+        self.__yc__ = yc # YieldCurve object
         self.rec_lag = rec_lag
         self.ids = []
         self.amnts = []
+        self.n_loans = defaultdict(int)
         self.payments = defaultdict(int)
         self.receipts = defaultdict(int)
+        self.PVs = defaultdict(int)
 
     def __gen_cfs__(self, id, amnt):
-        start = dt.datetime.strptime(self.__univ.loc[id, 'issue_d'].astype('str'), '%Y%m%d')
-        stop = dt.datetime.strptime(self.__univ.loc[id, 'last_pymnt_d'].astype('str'), '%Y%m%d')
+        start = dt.datetime.strptime(self.__univ__.loc[id, 'issue_d'].astype('str'), '%Y%m%d')
+        stop = dt.datetime.strptime(self.__univ__.loc[id, 'last_pymnt_d'].astype('str'), '%Y%m%d')
+        self.n_loans[start] += 1
         self.payments[start] -= amnt
-        ratio = amnt / self.__univ.loc[id, 'funded_amnt']
+        ratio = amnt / self.__univ__.loc[id, 'funded_amnt']
+        self.PVs[start] += ratio * self.__univ__.loc[id, 'PV']
         months = int((stop - start).days * 12 / 365)
         # adjust payments for LC's 1% fee
-        pmts = ratio * (self.__univ.loc[id, 'total_pymnt'] - self.__univ.loc[id, 'recoveries']) * (1 - lc_fee)
+        pmts = ratio * (self.__univ__.loc[id, 'total_pymnt'] - self.__univ__.loc[id, 'recoveries']) * (1 - lc_fee)
         if months == 0:
             self.receipts[start] += pmts
         else:
@@ -56,7 +63,7 @@ class Portfolio:
                 d = d + dt.timedelta(days=31)
                 d = dt.datetime(d.year, d.month, 1)
                 self.receipts[d] += pmts / months
-        recs = ratio * self.__univ.loc[id, 'recoveries'] # No LC fee on recoveries
+        recs = ratio * self.__univ__.loc[id, 'recoveries'] # No LC fee on recoveries
         if recs > 0:
             d = stop + dt.timedelta(days = 31*rec_lag)
             d = dt.datetime(d.year, d.month, 1)
@@ -71,12 +78,15 @@ class Portfolio:
         dates = sorted(set(self.payments.keys()).union(set(self.receipts.keys())))
         dates = [dat for dat in dates if dat <= d]
         if dates == []:
-            df1 = pd.DataFrame({'payments': 0, 'receipts': 0, 'net': 0}, index=[d])
+            df1 = pd.DataFrame({'payments': 0, 'receipts': 0, 'net': 0, 'PV': 0}, index=[d])
         else:
-            s1 = pd.Series([self.payments[dat] for dat in dates if dat <= d], index=dates, name='payments')
-            s2 = pd.Series([self.receipts[dat] for dat in dates if dat <= d], index=dates, name='receipts')
-            df1 = pd.concat([s1, s2], axis=1)
+            s1 = pd.Series([self.n_loans[dat] for dat in dates if dat <= d], index=dates, name='n_loans')
+            s2 = pd.Series([self.payments[dat] for dat in dates if dat <= d], index=dates, name='payments')
+            s3 = pd.Series([self.receipts[dat] for dat in dates if dat <= d], index=dates, name='receipts')
+            s4 = pd.Series([self.PVs[dat] for dat in dates if dat <= d], index=dates, name='PVs')
+            df1 = pd.concat([s1, s2, s3, s4], axis=1)
             df1['net'] = df1['payments'] + df1['receipts']
+            df1 = df1[['n_loans', 'payments', 'receipts', 'net', 'PVs']]
         return(df1)
 
     def spent(self, d):
@@ -93,28 +103,7 @@ class Portfolio:
     def FV(self, d):
         # future value of net cashflows to date d
         df4 = self.get_cashflows(d)
-        df4['days'] = (d - df4.index).days
-
-        def get_rate(term, as_of):
-            df = self.__rates
-            idx = df.index.get_loc(as_of, method='pad')
-            if term <= 15:
-                r = df.iloc[idx, 6]  # FF
-            elif term <= 365:
-                r = df.iloc[idx, 5]  # 3mo LIBOR
-            elif term <= 365 * 2.5:
-                r = df.iloc[idx, 2]  # 2yr T
-            elif term <= 365 * 4:
-                r = df.iloc[idx, 3]  # 3yr T
-            else:
-                r = df.iloc[idx, 4]  # 5yr T
-            return(r)
-
-        df4['dates'] = df4.index
-        df4['rates'] = df4[['days', 'dates']].apply(lambda x: get_rate(x[0], x[1]), axis=1)
-        # Assume semi-annual compounding for all rates (true for the T rates, fudge for money-market)
-        FV = df4['net'] * (1 + df4['rates']/200) ** (2*df4['days']/365)
-        return(FV.sum())
+        return(yc.FV(df4.index, df4['net']))
 
     def IRR(self, d):
         # IRR of net cashflows to date d
@@ -128,8 +117,12 @@ class Portfolio:
 
 # Read in data
 accept = pd.read_pickle('../derivedData/train.pkl')
+# Remove rows we don't understand
+accept = accept[accept['last_pymnt_d'] >= accept['issue_d']]
+# set up the Yield Curve
 rates = pd.read_csv('../macroData/rates.csv', index_col=0)
 rates.index = pd.to_datetime(rates.index)
+yc = YieldCurve(rates)
 
 # Split target from attributes and normalise attribs
 y = accept['PV'].to_numpy()
@@ -151,8 +144,8 @@ ct = make_column_transformer((StandardScaler(), cols_to_scale), remainder='passt
 
 # Initialise the model and two empty portfolios
 boost = GradientBoostingRegressor(subsample=0.1, verbose=0, warm_start=False)
-port_sel = Portfolio(accept, rates, rec_lag) # will track the best loans we select
-port_rand = Portfolio(accept, rates, rec_lag) # will track a randomly-selected portfolio
+port_sel = Portfolio(accept, yc, rec_lag) # will track the best loans we select
+port_rand = Portfolio(accept, yc, rec_lag) # will track a randomly-selected portfolio
 
 # Run the sim for each month that loans were issued
 dates = np.sort(X.loc[X['issue_d'] >= first_invest, 'issue_d'].unique())
@@ -185,16 +178,50 @@ for i, date in enumerate(dates):
     [port_rand.add(ix, max_per_loan)
          for ix in np.random.choice(idx, bought, replace=False)]
 
+# Generate results
 fv_date = max(port_sel.receipts.keys())
+res_sel = port_sel .get_cashflows(fv_date)
+res_rand = port_rand .get_cashflows(fv_date)
+results = res_sel.join(res_rand, how='outer', lsuffix='_sel', rsuffix='_rand')
+results['PVs_sel'] = results['PVs_sel'] / results['n_loans_sel']
+results['PVs_rand'] = results['PVs_rand'] / results['n_loans_rand']
+offered = accept[['issue_d', 'funded_amnt']].copy()
+offered['issue_d'] = offered['issue_d'].apply(lambda i: dt.datetime.strptime(str(i), '%Y%m%d'))
+offered = offered.groupby('issue_d').agg(['count', 'sum'])
+offered.columns = offered.columns.get_level_values(1)
+results = results.join(offered)
+results['pct_of_loans'] = results['n_loans_sel'] / results['count']
+results['pct_of_not'] = -results['payments_sel'] / results['sum']
+results['year'] = results.index.map(lambda d: d.year)
+results = results.groupby('year').agg({'n_loans_sel': sum, 'pct_of_loans': np.mean, 'pct_of_not': np.mean,
+                                       'payments_sel': sum, 'receipts_sel': sum, 'net_sel': sum,
+                                       'receipts_rand': sum, 'net_rand': sum, 'PVs_sel': np.mean, 'PVs_rand': np.mean})
+
+# Output results
 print('-'*25)
 print(f'Total funded: {port_sel.drawdown(fv_date):,.0f}')
 print(f'Total FV of selected: {port_sel.FV(fv_date):,.0f}')
 print(f'Total FV of rand: {port_rand.FV(fv_date):,.0f}')
 print(f'IRR of selected: {port_sel.IRR(fv_date):.1%}')
 print(f'IRR of rand: {port_rand.IRR(fv_date):.1%}')
+print('-'*25)
 
-# Need to add more metrics:
-# - what % of loans on offer did we buy (by count and by notional)
-# - what was our annual trading P&L (i.e. PV - spent)
+pio.renderers.default = "browser"
+results = results.reset_index()
+heads = ['Year', '# Loans', '% LC by Count', '% LC by Value', 'Pmts', 'Rcts (sel)', 'Net (sel)',
+         'Rcts (rnd)', 'Net (rnd)', 'PV/loan (sel)', 'PV/loan (rnd)']
+fig = go.Figure(data=[go.Table(
+    header=dict(values=heads,
+                fill_color='paleturquoise',
+                align='center'),
+    cells=dict(values=[results[col] for col in results.columns],
+               fill_color='lavender',
+               align='right',
+               format = [[None], [None], ['.1%'], ['.1%'],
+                        [',.0f'], [',.0f'], [',.0f'], [',.0f'],
+                        [',.0f'], [',.0f'], [',.0f']]))
+])
+fig.show()
+
 # Tweak the model: hyper param tunning and pruning
 # Fit tactic params
---
+++ Simon/YieldCurve.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import datetime as dt
+# Defies a YieldCurve object with methods PV and FV
+
+class YieldCurve(object):
+    def __init__(self, rates, verbose=False):
+        self.__rates__ = rates
+        self.__verbose__ = verbose
+
+    def get_rate(self, as_of, days):
+        df = self.__rates__
+        idx = df.index[df.index.get_loc(as_of, method='pad')]
+        if days <= 15:
+            r = df.loc[idx, 'FEDL01 Index']
+        elif days <= 365:
+            r = df.loc[idx, 'US0003M Index']
+        elif days <= 365 * 2.5:
+            r = df.loc[idx, 'GT2 Govt']
+        elif days <= 365 * 4:
+            r = df.loc[idx, 'GT3 Govt']
+        else:
+            r = df.loc[idx, 'GT5 Govt']
+        return(r/100)
+
+    def PV(self, dates, cfs):
+        d = min(dates)
+        days = (dates - d).days
+        rates = days.map(lambda x: self.get_rate(d, x))
+        if self.__verbose__:
+            print('Days')
+            print(days)
+            print('Rates')
+            print(rates)
+        return((cfs / (1 + rates/2)**(2*days/365)).sum())
+
+    def FV(self, dates, cfs):
+        d = max(dates)
+        days = (d - dates).days
+        rates = days.map(lambda x: self.get_rate(d - dt.timedelta(days=x), x))
+        if self.__verbose__:
+            print('Days')
+            print(days)
+            print('Rates')
+            print(rates)
+        return((cfs * (1 + rates/2)**(2*days/365)).sum())
